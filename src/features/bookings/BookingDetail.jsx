c7c65cd18782a73ff8c44703369241cb6feb8ddb
Mucahit Bedir <mucahitbedirdev@gmail.com>
2025-02-26 19:00:19 +0300

    add check-in functionality for bookings

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -11,6 +11,8 @@ import ButtonText from '../../ui/ButtonText';
 import { useMoveBack } from '../../hooks/useMoveBack';
 import { useBooking } from './useBooking';
 import Spinner from '../../ui/Spinner';
+import { useNavigate } from 'react-router-dom';
+import { HiArrowDownOnSquare } from 'react-icons/hi2';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -22,6 +24,7 @@ function BookingDetail() {
   const { booking, isLoading } = useBooking();
 
   const moveBack = useMoveBack();
+  const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
 
@@ -46,6 +49,15 @@ function BookingDetail() {
       <BookingDataBox booking={booking} />
 
       <ButtonGroup>
+        {status === 'unconfirmed' && (
+          <Button
+            icon={<HiArrowDownOnSquare />}
+            onClick={() => navigate(`/checkin/${bookingId}`)}
+          >
+            Check In
+          </Button>
+        )}
+
         <Button variation='secondary' onClick={moveBack}>
           Back
         </Button>
