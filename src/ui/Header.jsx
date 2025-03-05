e61e0e5f314bfc56642957e156fdddf8b28144c0
Mucahit Bedir <mucahitbedirdev@gmail.com>
2025-03-05 18:30:23 +0300

    feat: add dark mode toggle functionality

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,9 +3,9 @@ import HeaderMenu from './HeaderMenu';
 import UserAvatar from '../features/authentication/UserAvatar';
 
 const StyledHeader = styled.header`
-  background-color: var(--color-gray-0);
+  background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
-  border-bottom: 1px solid var(--color-gray-100);
+  border-bottom: 1px solid var(--color-grey-100);
 
   display: flex;
   gap: 2.4rem;
